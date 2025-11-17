07f14f5dec8ca325658d36bee0c21e4bb17fbc9f
Sumangal Chhetri <132873278+SumangalChhetri@users.noreply.github.com>
2025-11-17 10:59:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello charizard goo World! My DevOps Project is LIVE!');
+  res.send('Hello goo World! My DevOps Project is LIVE!');
 });
 
 app.listen(PORT, () => {
